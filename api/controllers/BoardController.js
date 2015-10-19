232cafbaf54081a7d1b0f303979cef237c3b3b54
pooler22 <rudypawel12@gmail.com>
2015-10-19 17:17:17 +0200

    dodanie zmiany nazwy tablicy

--- api/controllers/BoardController.js
+++ api/controllers/BoardController.js
@@ -34,12 +34,29 @@ module.exports = {
       });
     });
   },
-  index: function(req,res, next){
-    Board.find(function foundBoards (err,boards) {
+  index: function(req,res, next) {
+    Board.find(function foundBoards(err, boards) {
       if (err) return next(err);
       res.view({
         boards: boards
       });
+    });
+  },
+  edit: function(req,res,next){
+    Board.findOne(req.params['id'], function foundBoard(err, board){
+      if (err) return next(err);
+      if (!board) return next();
+      res.view({
+        board: board
+      });
+    });
+  },
+  update: function(req, res, next){
+    Board.update(req.params['id'], req.params.all(), function updateBoard(err){
+      if(err){
+        return res.redirect('/board/edit/' + req.param('id'));
+      }
+      res.redirect('/board/show/' + req.param('id'));
     });
   }
 };
