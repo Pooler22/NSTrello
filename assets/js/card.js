44b583416c693125b2d09c230d88e73490dc1539
Krystian Krzywda <krystiankrzywda22@gmail.com>
2015-11-19 13:05:04 +0100

    poprawione dodawanie zadan

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 
-  $(".form-create-card").submit(function(e) {
+    $('.new-card').click(function(){
+       $('.new-card').addClass('form-hidden');
+       $(this).removeClass('form-hidden');
+        console.log($(this).children('textarea').length);
 
+    });
+
+    $(document).mouseup(function (e)
+    {
+        var container = $(".list ");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            $('.new-card').addClass('form-hidden');
+        }
+    });
+
+  $(".form-create-card").submit(function(e) {
+    var textarea = $(this).children('.card-composer').children('.list-card').children('.list-card-details').children('.list-card-composer-textarea');
     var url = $(this).attr('action');
     var header = $(this).children('.card-composer').children('.list-card').children('.list-card-details').children('.card-title').val();
     var owner = $(this).children('.card-composer').children('.cu-clearfix').children('.list-id').attr('value');
@@ -9,13 +27,19 @@ $(document).ready(function() {
       type: "POST",
       url: url,
       data: {
-        header: header,
+        header: textarea.val(),
         owner: owner
       },
       success: function(data) {
         if (data.result === true) {
           console.log(data);
-          $(".new-card").append(".card-show");
+            $('[data-list-id="'+owner+'"] .card-list').append('<div class="card">'+
+            '<input type="hidden" name="owner" value="">'+
+            '<div class="card-title">'+header+'</div>'+
+            '</div>');
+
+            textarea.val("").focus();
+
         } else {
           console.log(data);
         }
